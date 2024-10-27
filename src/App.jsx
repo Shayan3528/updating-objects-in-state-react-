ba38c74280d4ac6fde79f7a908c339d8b4aa218b
Shayan <35280babu@gmail.com>
2024-10-27 15:12:46 +0600

    making other changes to an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-let nextId = 3;
-
-const initialArtists = [
-  {id:0,name:'Shayan'},
-  {id:1,name:'papon'},
-  {id:2, name:'kishor'}
+const initialList = [
+  { id: 0, title: "Big Bellies" },
+  { id: 1, title: "Lunar Landscape" },
+  { id: 2, title: "Terracotta Army" },
 ];
 
-export default function App(){
-  const [name,setName] = useState('');
-  const [artists,setArtists]= useState(initialArtists);
-  function handleClick(){
-    const insertAt = 1;
-    const nextArtists = [
-      ...artists.slice(0,insertAt),
-      {id:nextId++, name:name},
-      ...artists.slice(insertAt)
-    ];
-    setArtists(nextArtists);
-    setName('');
+export default function App() {
+  const [list, setList] = useState(initialList);
+
+  function handleClick() {
+    const nextList = [...list];
+    nextList.reverse();
+    setList(nextList);
   }
 
-  return(
+  return (
     <>
-      <input value= {name} onChange = {e=>setName(e.target.value)} />
-      <button onClick = {handleClick}> 
-        Insert
-      </button>
+      <button onClick={handleClick}> Reverse</button>
       <ul>
-        {artists.map(artist => (
-          <li key = {artist.id}> {artist.name} </li>
+        {list.map((a) => (
+          <li key={a.id}> {a.id+1}     {a.title} </li>
         ))}
       </ul>
     </>
-  )
+  );
 }
-
